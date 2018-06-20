f2e10ebaf97db9d9e638267e98932ff3769f2f85
Hans-Peter Bergner <Hans-Peter Bergner hapebe@gmx.de>
2018-06-20 12:18:55 +0200

    Leitungswiderstand berechnen, Basisversion fertig

--- itw-cpp/lange-leitung.cpp
+++ itw-cpp/lange-leitung.cpp
@@ -1,9 +1,15 @@
+// zum Abschalten des Debug-Modus die nächste Zeile bitte komplett auskommentieren!
+// #define DEBUG 1
+
 #include <iostream>
+#include <iomanip>
+#include <regex>
 
 using namespace std;
 
 class Stromkreis {
 	private:
+		string material;
 		double I; // Stromstärke
 		double l; // Leitungslänge (einfacher Weg!)
 		double rho; // spezifischer Widerstand
@@ -19,7 +25,7 @@ class Stromkreis {
 			} else if (material.compare("Aluminium")==0) {
 				this->rho = 0.0265; // Ohm * mm^2 / m
 			} else if (material.compare("Messing")==0) {
-				this->rho = 0.07; // Ohm * mm^2 / m
+				this->rho = 7e-2; // Ohm * mm^2 / m  (e steht für "* 10 ^ ...")
 			} else if (material.compare("Konstantan")==0) {
 				this->rho = 0.5; // Ohm * mm^2 / m
 			} else if (material.compare("Meerwasser")==0) {
@@ -28,6 +34,9 @@ class Stromkreis {
 				cout << material << " ist ein unbekanntes Material." << endl;
 				exit(1);
 			}
+			this->material = material;
+			
+			// Default-Werte für die numerischen Angaben:
 			I = 1.0; // A
 			l = 100; // m
 			A = 2.5; // mm^2
@@ -36,6 +45,7 @@ class Stromkreis {
 		void setL(double l) { this->l = l; };
 		void setRho(double rho) { this->rho = rho; };
 		void setA(double A) { this->A = A; };
+		string getMaterial(void) { return material; }
 		double getI(void) { return I; }
 		double getL(void) { return l; }
 		double getRho(void) { return rho; }
@@ -46,30 +56,90 @@ class Stromkreis {
 		}
 		double getLeitungsWiderstand() {
 			// R = rho * l / A
-			// die *2 ist für reale Leitungen, die 2 Adern haben!
+			// das *2 ist für reale Leitungen, die 2 Adern haben!
 			return rho * l / A * 2;
 		}
 		
 		void statusAusgeben(void) {
-			cout << "Stromkreis: I=" << I << "A, ";
+			cout << "Stromkreis: Material ist " << material << ", ";
+			cout << "rho=" << rho << "Ohm*mm^2, ";
 			cout << "l=" << l << "m, ";
-			cout << "A=" << A << "mm², ";
-			cout << "rho=" << rho << "Ohm*mm^2";
+			cout << "A=" << A << "mm^2, ";
+			cout << "I=" << I << "A";
 			cout << endl;
 		}
 	
 };
 
+bool istGueltigeZahl(string s) {
+	// der reguläre Ausdruck setzt sich aus folgenden Teilen zusammen:
+	// 1. "-" als Vorzeichen, oder nicht
+	// 2. entweder eine einzelne Null
+	//    ODER eine 1..9 gefolgt von beliebig vielen 0..9
+	// 3. entweder nichts, ODER ein Muster wie folgt:
+	//    3a. ein "," oder ein "."
+	//    3b. beliebig viele 0..9
+    regex regexp("-?(0|([1-9][0-9]*))([,\\.][0-9]*)?");
+    return regex_match(s, regexp);
+}
+
+double fliessKommaEingabe(void) {
+	string eingabe;
+	bool okay = false;
+	while (!okay) {
+		cin >> eingabe;
+		
+		if (istGueltigeZahl(eingabe)) {
+			// ggf. vorhandenes "," in einen "." umwandeln, damit stod funktioniert:
+			int kommaPos = eingabe.find_first_of(",");
+			if (kommaPos != string::npos) {
+				eingabe.replace(kommaPos, 1, ".");
+			}
+			
+			return stod(eingabe);
+		} else {
+			cout << endl;
+			cout << "\"" << eingabe << "\" ist keine gueltige Zahl." << endl;
+			cout << "Bitte wiederholen Sie die Eingabe: ";
+		}
+	}
+}
+
 int main() {
+	cout << "Berechnung des Spannungsabfalls an einer Zuleitung." << endl;
+	cout << "Gegeben ist neben den Eigenschaften der Zuleitung eine konstante Stromstaerke." << endl;
+	cout << endl;
+	
+#ifdef DEBUG
+	cout << "Bitte geben Sie eine Kommazahl ein: ";
+	double d = fliessKommaEingabe();
+	cout << "Eingabe fertig: " << setprecision(12) << d << endl;
+	exit(0);
+#endif
+
 	Stromkreis s("Kupfer");
 	// Rho ist damit gesetzt!
 	s.setI(4.7);
 	s.setL(134);
 	s.setA(2.5);
 	
+	cout << "Bitte geben Sie die Stromstaerke in Ampere ein: ";
+	s.setI(fliessKommaEingabe());
+	
+	cout << "Bitte geben Sie die Laenge der Leitung in Metern ein (sie wird im Programm noch mit 2 multipliziert, um ein 2-adriges Kabel zu betrachten): ";
+	s.setL(fliessKommaEingabe());
+	
+	cout << "Bitte geben Sie den Leitungsquerschnitt in Quadratmillimetern ein: ";
+	s.setA(fliessKommaEingabe());
+	
+	cout << endl;
+	cout << "Wir haben also den folgenden Fall:" << endl;
 	s.statusAusgeben();
+	
+	cout << "Es ergeben sich die folgenden Werte:" << endl;
 	cout << "Leitungswiderstand: " << s.getLeitungsWiderstand() << "Ohm" << endl;
 	cout << "Spannungsabfall: " << s.getSpannungsAbfall() << "V" << endl;
+	cout << "Verlustleistung: " << (s.getSpannungsAbfall()* s.getI()) << "W" << endl;
 	
     return 0;
 }
